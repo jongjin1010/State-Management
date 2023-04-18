f914553d8f26a7fb0feb8971050c9eba1cee9848
jongjin1010 <jongjin024@gmail.com>
2023-04-19 08:49:01 +0900

    add: todo types

--- redux/src/types/todo.type.ts
+++ redux/src/types/todo.type.ts
@@ -3,3 +3,19 @@ export interface TodoData {
   text: string;
   done?: boolean;
 }
+
+export interface TodoItemProps {
+  todo: TodoData;
+  onToggle: (id: number) => void;
+}
+
+export interface TodoListProps {
+  todos: TodoData[];
+  onToggle: (id: number) => void;
+}
+
+export interface TodosProps {
+  todos: TodoData[];
+  onCreate: (text: string) => void;
+  onToggle: (id: number) => void;
+}
