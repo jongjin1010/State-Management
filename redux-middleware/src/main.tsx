659a99e1f9788dcc77bfedc912df91ebca7d1099
jongjin1010 <jongjin024@gmail.com>
2023-04-24 14:08:33 +0900

    add: redux-thunk

--- redux-middleware/src/main.tsx
+++ redux-middleware/src/main.tsx
@@ -6,8 +6,9 @@ import rootReducer from "./modules";
 import { Provider } from "react-redux";
 import myLogger from "./middlewares/myLogger";
 import logger from "redux-logger";
+import ReduxThunk from "redux-thunk";
 
-const store = createStore(rootReducer, applyMiddleware(logger));
+const store = createStore(rootReducer, applyMiddleware(ReduxThunk, logger));
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <Provider store={store}>
